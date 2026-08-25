01dace48a661a0a947fffe7d241286f5137a9e27
raimu <rafa.gaj007@gmail.com>
2026-08-25 16:24:17 +0200

    created local network scanning

---
+++ core/discovery.py
@@ -0,0 +1,43 @@
+import socket
+import ipaddress
+import subprocess
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def generate_subnet_ips(ip,mask):
+    addresses = ipaddress.ip_network(f"{ip}/{mask}",strict=False)
+    ip_list = [str(host) for host in addresses.hosts()]
+
+    return ip_list
+
+# currently unused
+def ping_host(ip):
+    ports_to_check = [80, 443, 22, 445, 5353, 8080, 62078]
+    for port in ports_to_check:
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.settimeout(0.3)
+        result = s.connect_ex((ip,port))
+        s.close()
+        if result == 0:
+            return True
+    return False
+
+def ping_icmp(ip):
+    cmd = ["ping", "-c", "3", "-W", "1", ip]
+    result = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return ip, (result.returncode == 0)
+
+def scan_network(ip_list):
+    active_hosts = []
+
+    print(f"Scanning {len(ip_list)} addresses...")
+
+    with ThreadPoolExecutor(max_workers=30) as executor:
+        futures = [executor.submit(ping_icmp, ip) for ip in ip_list]
+
+        for future in as_completed(futures):
+            ip, is_alive = future.result()
+            if is_alive:
+                print(f"Active host: {ip}")
+                active_hosts.append(ip)
+
+    return active_hosts
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import argparse
 from core.network_info import get_network_info
+from core.discovery import scan_network, generate_subnet_ips
 
 def main():
     parser = argparse.ArgumentParser(description="Network Scanner CLI Tool")
     parser.add_argument("-i","--info",
                         action="store_true",
                         help="display local network info")
+
+    parser.add_argument("-s", "--scan",
+                        action="store_true",
+                        help="scan local network for active devices")
 
     args = parser.parse_args()
 
@@ -17,6 +22,18 @@ def main():
         print(f"IP Address: {info['ip']}")
         print(f"Mask      : {info['mask']}")
         print(f"MAC       : {info['mac']}")
+
+    elif args.scan:
+        print("Scanning local network...")
+        info = get_network_info()
+        hosts_to_scan = generate_subnet_ips(info["ip"], info["mask"])
+
+        active_hosts = scan_network(hosts_to_scan)
+
+        print("ACTIVE HOSTS:")
+        for host in active_hosts:
+            print(f" -> {host}")
+        print(f"Detected: {len(active_hosts)} active hosts.\n")
 
     else:
         parser.print_help()
